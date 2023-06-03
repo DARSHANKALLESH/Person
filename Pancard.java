2dddc82dc1f7c84a5496e85e12d444e8006d332b
DARSHANKALLESH <darshannkdarshi@gmail.com>
2023-06-03 12:23:19 +0530

    first

--- Pancard.java
+++ Pancard.java
@@ -1,5 +1,45 @@
 package personPancard.com;
 
-public class Pancard {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Pancard {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String Adress;
+	private long phno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return Adress;
+	}
+	public void setAdress(String adress) {
+		Adress = adress;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	@Override
+	public String toString() {
+		return "Pancard [id=" + id + ", name=" + name + ", Adress=" + Adress + ", phno=" + phno + "]";
+	}
+	
+	
 }
